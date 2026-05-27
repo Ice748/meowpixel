75ac41767db22b57efb55e81ade9db61154acada
Ice748 <dvinogradov174@gmail.com>
2026-05-27 21:41:14 +0300

    Update main.c
    meowpixel v0.1 beta
    
    added:
    main window;
    functions main, setup, unsetup, functional and draw;
    interface (field).

--- src/main.c
+++ src/main.c
@@ -1,1 +1,56 @@
+#include "raylib.h"
 
+
+void setup(void);
+void unsetup(void);
+
+void functional(void);
+void draw(void);
+
+int pixels[100];
+
+
+int main(void) {
+  setup();
+  while (!WindowShouldClose()) {
+    functional();
+    draw();
+  }
+  unsetup();
+  return 0;
+}
+
+
+void setup(void) {
+  InitWindow(1250, 1250, "meowpixel v0.1 beta");
+}
+
+void unsetup(void) {
+  CloseWindow();
+}
+
+
+void functional(void) {
+
+}
+
+void draw(void) {
+  BeginDrawing();
+    ClearBackground((Color){255, 255, 245, 255});
+
+    for (int i = 0; i < 100; i++) {
+      int col = i % 10;
+      int row = i / 10;
+
+      int posX = 125 + (col * 100);
+      int posY = 125 + (row * 100);
+
+      DrawRectangle(posX, posY, 100, 100, pixels[i] ? BLACK : WHITE);
+    }
+
+    for (int i = 125; i <= 1125; i += 100)
+        DrawLine(i, 125, i, 1125, BLACK);
+    for (int i = 125; i <= 1125; i += 100)
+        DrawLine(125, i, 1125, i, BLACK);
+  EndDrawing();
+}
