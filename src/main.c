a8b3e340850a98c95bd4354654cf1cf497c098e3
Ice748 <dvinogradov174@gmail.com>
2026-05-28 12:34:48 +0300

    Update main.c
    meowpixel v0.2 beta
    
    added:
    export mode (F1)
    clear screen button (Space)
    hints
    image export system

--- src/main.c
+++ src/main.c
@@ -10,11 +10,14 @@
 int pixels[TOTAL_PIXELS];
 
 int pixel_size;
+int pixel_size_full;
 
 int offset_left;
 int offset_right;
 int offset_top;
 int offset_bottom;
+
+int export_mode = 0;
 
 void setup(void);
 void unsetup(void);
@@ -35,10 +38,11 @@ int main(void) {
 
 
 void setup(void) {
-  InitWindow(1250, 1250, "meowpixel v0.1 beta");
+  InitWindow(1250, 1250, "meowpixel v0.2 beta");
   SetTargetFPS(60);
 
   pixel_size = 1000 / MAX(GRID_WIDTH, GRID_HEIGHT);
+  pixel_size_full = MAX(GetScreenWidth(), GetScreenHeight()) / MAX(GRID_WIDTH, GRID_HEIGHT);
 
   offset_left = (GetScreenWidth() - (GRID_WIDTH * pixel_size)) / 2;
   offset_right = offset_left + (GRID_WIDTH * pixel_size);
@@ -52,8 +56,8 @@ void unsetup(void) {
 
 
 void functional(void) {
-  if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) || IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) {
-    int click_value = IsMouseButtonPressed(MOUSE_BUTTON_LEFT) ? 1 : 0;
+  if (!export_mode && (IsMouseButtonDown(MOUSE_BUTTON_LEFT) || IsMouseButtonDown(MOUSE_BUTTON_RIGHT))) {
+    int click_value = IsMouseButtonDown(MOUSE_BUTTON_LEFT) ? 1 : 0;
 
     for (int i = 0; i < TOTAL_PIXELS; i++) {
       int col = i % GRID_WIDTH;
@@ -67,30 +71,73 @@ void functional(void) {
         break;
       }
     }
+  } else if (!export_mode && IsKeyPressed(KEY_SPACE)) {
+    for (int i = 0; i < sizeof(pixels) / sizeof(pixels[0]); i++)
+      pixels[i] = 0;
+  } else if (IsKeyPressed(KEY_F1)) {
+    export_mode = !export_mode;
+  } else if (export_mode && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+    Image img = GenImageColor(GRID_WIDTH * 10, GRID_HEIGHT * 10, BLANK);
+
+    for (int i = 0; i < TOTAL_PIXELS; i++) {
+      int col = i % GRID_WIDTH;
+      int row = i / GRID_WIDTH;
+
+      Color clr = pixels[i] ? BLACK : WHITE;
+      for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+          int x = (col * 10) + j;
+          int y = (row * 10) + i;
+
+          ImageDrawPixel(&img, x, y, clr);
+        }
+      }
+    }
+
+    ExportImage(img, "exports/image.png");
+    UnloadImage(img);
   }
 }
 
 
 void draw(void) {
   BeginDrawing();
-    ClearBackground((Color){255, 255, 245, 255});
+    if (!export_mode) {
+      ClearBackground((Color){255, 255, 245, 255});
 
-    for (int i = 0; i < TOTAL_PIXELS; i++) {
-      int col = i % GRID_WIDTH;
-      int row = i / GRID_WIDTH;
+      for (int i = 0; i < TOTAL_PIXELS; i++) {
+        int col = i % GRID_WIDTH;
+        int row = i / GRID_WIDTH;
 
-      int posX = offset_left + (col * pixel_size);
-      int posY = offset_top + (row * pixel_size);
+        int posX = offset_left + (col * pixel_size);
+        int posY = offset_top + (row * pixel_size);
 
-      DrawRectangle(posX, posY, pixel_size, pixel_size, pixels[i] ? BLACK : WHITE);
+        DrawRectangle(posX, posY, pixel_size, pixel_size, pixels[i] ? BLACK : WHITE);
+      }
+
+      for (int i = offset_left; i <= offset_right; i += pixel_size)
+          DrawLine(i, offset_top, i, offset_bottom, BLACK);
+      for (int i = offset_top; i <= offset_bottom; i += pixel_size)
+          DrawLine(offset_left, i, offset_right, i, BLACK);
+
+      DrawText("LMB to draw", GetScreenWidth() - MeasureText("LMB to draw", 16) - 5, GetScreenHeight() - 16 * 5 - 5, 16, BLACK);
+      DrawText("RMB to erase", GetScreenWidth() - MeasureText("RMB to erase", 16) - 5, GetScreenHeight() - 16 * 4 - 5, 16, BLACK);
+      DrawText("Space to clear screen", GetScreenWidth() - MeasureText("Space to clear screen", 16) - 5, GetScreenHeight() - 16 * 3 - 5, 16, BLACK);
+      DrawText("F1 to toggle export mode", GetScreenWidth() - MeasureText("F1 to toggle export mode", 16) - 5, GetScreenHeight() - 16 * 1 - 5, 16, BLACK);
+
+      DrawFPS(0, 0);
+    } else if (export_mode) {
+      ClearBackground((Color){255, 255, 255, 255});
+
+      for (int i = 0; i < TOTAL_PIXELS; i++) {
+        int col = i % GRID_WIDTH;
+        int row = i / GRID_WIDTH;
+
+        int posX = col * pixel_size_full;
+        int posY = row * pixel_size_full;
+
+        DrawRectangle(posX, posY, pixel_size_full, pixel_size_full, pixels[i] ? BLACK : WHITE);
+      }
     }
-
-    for (int i = offset_left; i <= offset_right; i += pixel_size)
-        DrawLine(i, offset_top, i, offset_bottom, BLACK);
-
-    for (int i = offset_top; i <= offset_bottom; i += pixel_size)
-        DrawLine(offset_left, i, offset_right, i, BLACK);
-
-    DrawFPS(0, 0);
   EndDrawing();
 }
